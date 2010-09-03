a9bf47293fec4462d50e351b55df03388a2e7e69
Isaiah <issaria@gmail.com>
2010-09-03 22:09:55 +0800

    substitute #spawn with #new

--- test/object_daddy_helpers.rb
+++ test/object_daddy_helpers.rb
@@ -10,12 +10,12 @@ module ObjectDaddyHelpers
   end
 
   def User.spawn_with_protected(attributes={})
-    User.spawn(attributes)
+    User.new(attributes)
   end
 
   # Generate the default Query
   def Query.generate_default!(attributes={})
-    query = Query.spawn(attributes)
+    query = Query.new(attributes)
     query.name ||= '_'
     query.save!
     query
@@ -23,7 +23,7 @@ module ObjectDaddyHelpers
 
   # Generate an issue for a project, using it's trackers
   def Issue.generate_for_project!(project, attributes={})
-    issue = Issue.spawn(attributes) do |issue|
+    issue = Issue.new(attributes) do |issue|
       issue.project = project
     end
     issue.tracker = project.trackers.first unless project.trackers.empty?
