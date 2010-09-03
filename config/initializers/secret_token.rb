644d889335dae902631c2f3640ee2eaa7a9160cc
Isaiah <issaria@gmail.com>
2010-09-03 22:16:41 +0800

    added forgotten rails3 files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedmineFork::Application.config.secret_token = '3af9ee1d2d3ce2a3cfb72fbf58c897aa1fa4fe36b1187a6ab4a1f1cad2902bbaf457ab2256fbd41f29046f075be58f7158bff3f5c04b1821310316311fa0f109'
