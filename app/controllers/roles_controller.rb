dcd27754b823bfda6f2a33830fa638a1e20410dc
Isaiah <issaria@gmail.com>
2010-09-09 22:48:02 +0800

    action create instead of post :new

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -43,6 +43,16 @@ class RolesController < ApplicationController
     @roles = Role.find :all, :order => 'builtin, position'
   end
 
+  def create
+    @role = Role.new(params[:role] || {:permissions => Role.non_member.permissions})
+    # workflow copy
+    if !params[:copy_workflow_from].blank? && (copy_from = Role.find_by_id(params[:copy_workflow_from]))
+      @role.workflows.copy(copy_from)
+    end
+    flash[:notice] = l(:notice_successful_create)
+    redirect_to :action => 'index'
+  end
+
   def edit
     @role = Role.find(params[:id])
     if request.post? and @role.update_attributes(params[:role])
